9d0dae774d594645dcca3eaaf976bc104f61a7f8
Volkov Dmitrij <Dmitr.Volkov@cft.ru>
2024-06-10 20:20:43 +0700

    add loader on profile page

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,10 +1,11 @@
+import { ReloadIcon } from '@radix-ui/react-icons'
 import { I18nText } from '@/components/common'
 import { Typography } from '@/components/ui'
 import { ProfileForm } from './components/ProfileForm/ProfileForm'
 import { useProfilePage } from './hooks/useProfilePage'
 
 export const ProfilePage = () => {
-  const { query } = useProfilePage()
+  const { state, query } = useProfilePage()
 
   return (
     <div className="flex h-screen">
@@ -12,7 +13,12 @@ export const ProfilePage = () => {
         <Typography tag="h1" variant="h1">
           <I18nText path="profile.title" />
         </Typography>
-        {/* // TODO loading state */}
+        {state.loading && (
+          <div className="mt-2 flex items-center gap-2">
+            <I18nText path="common.loading" />
+            <ReloadIcon className="h-4 w-4 animate-spin" />
+          </div>
+        )}
         {query.data?.data.data && <ProfileForm profile={query.data.data.data} />}
       </div>
     </div>

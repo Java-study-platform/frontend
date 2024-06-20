7f7562841da041480ca4f238f48ffa67a5b93736
Volkov Dmitrij <Dmitr.Volkov@cft.ru>
2024-06-20 19:41:39 +0700

    fix upload solution and user roles

--- generated/solution-api.ts
+++ generated/solution-api.ts
@@ -91,6 +91,7 @@ export interface DefaultResponseObject {
 
 export interface SendTestSolutionRequest {
   code?: string
+  taskId: string
 }
 
 export interface DefaultResponseSolutionDto {
--- src/pages/task/components/UploadSolutionSection/hooks/useUploadSolutionSection.ts
+++ src/pages/task/components/UploadSolutionSection/hooks/useUploadSolutionSection.ts
@@ -1,15 +1,17 @@
 import { usePostSolutionMutation } from '@/utils/api'
 import { useI18n } from '@/utils/contexts'
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { toast } from 'sonner'
 
 export const useUploadSolutionSection = () => {
+  const params = useParams<{ id: string }>()
   const i18n = useI18n()
   const [code, setCode] = React.useState<string>('')
   const postSolutionMutation = usePostSolutionMutation()
 
   const onUploadSolutionClick = async () => {
-    await postSolutionMutation.mutateAsync({ code })
+    await postSolutionMutation.mutateAsync({ code, taskId: params.id! })
     toast.success(i18n.formatMessage({ id: 'toast.solutionUploaded' }))
     setCode('')
   }

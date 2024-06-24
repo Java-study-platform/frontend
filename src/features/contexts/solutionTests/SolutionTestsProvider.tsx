b17303066ca8ae4a77e4aee57fa7ecab1d5f09bf
vitrivdolkom <vdy0555@gmail.com>
2024-06-24 18:14:41 +0700

    remove username from tests subscription

--- src/features/contexts/solutionTests/SolutionTestsProvider.tsx
+++ src/features/contexts/solutionTests/SolutionTestsProvider.tsx
@@ -4,7 +4,6 @@ import {
   SolutionDto,
   TestDto
 } from '@/generated/solution-api'
-import { useUserContext } from '@/utils/contexts'
 import { useStomp } from '@/utils/hooks'
 import { useQueryClient } from '@tanstack/react-query'
 import React from 'react'
@@ -19,7 +18,6 @@ export const solutionTestsQueryKey = (solutionId: string) => ['getSolutionTests'
 export const solutionQueryKey = (solutionId: string) => ['getSolution', solutionId]
 
 export const SolutionTestsProvider = ({ defaultSolutionId, children }: SolutionTestsProviderProps) => {
-  const userContext = useUserContext()
   const [solutionId, setSolutionId] = React.useState(defaultSolutionId)
 
   const stomp = useStomp()
@@ -29,41 +27,45 @@ export const SolutionTestsProvider = ({ defaultSolutionId, children }: SolutionT
   React.useEffect(() => {
     if (!stomp.isConnected) return () => {}
 
-    stomp.subscribe<TestDto>(
-      `/user/${userContext.user?.login}/solution/${solutionId}/test`,
-      (messageData) => {
-        queryClient.setQueryData<{ data: DefaultResponseListTestDto }>(
-          solutionTestsQueryKey(solutionId),
-          (oldData) => {
-            const prevTestsArray = oldData?.data.data ?? []
-
+    stomp.subscribe<TestDto>(`/solution/${solutionId}/test`, (messageData) => {
+      queryClient.setQueryData<{ data: DefaultResponseListTestDto }>(
+        solutionTestsQueryKey(solutionId),
+        (oldData) => {
+          const prevTestsArray = oldData?.data.data ?? []
+          if (prevTestsArray.find((test) => test.id === messageData.id)) {
             return {
               ...oldData,
-              data: { data: [...prevTestsArray, messageData] }
+              data: {
+                data: prevTestsArray.map((test) =>
+                  test.id === messageData.id ? { ...test, ...messageData } : test
+                )
+              }
             }
           }
-        )
-      }
-    )
 
-    stomp.subscribe<SolutionDto>(
-      `/user/${userContext.user?.login}/solution/${solutionId}`,
-      (messageData) => {
-        queryClient.setQueryData<{ data: DefaultResponseSolutionDto }>(
-          solutionQueryKey(solutionId),
-          (oldData) => ({
+          return {
             ...oldData,
-            data: { data: messageData }
-          })
-        )
-      }
-    )
+            data: { data: [...prevTestsArray, messageData] }
+          }
+        }
+      )
+    })
+
+    stomp.subscribe<SolutionDto>(`/solution/${solutionId}`, (messageData) => {
+      queryClient.setQueryData<{ data: DefaultResponseSolutionDto }>(
+        solutionQueryKey(solutionId),
+        (oldData) => ({
+          ...oldData,
+          data: { data: messageData }
+        })
+      )
+    })
 
     return () => {
-      stomp.unsubscribe(`/user/${userContext.user?.login}/solution/${solutionId}`)
-      stomp.unsubscribe(`/user/${userContext.user?.login}/solution/${solutionId}/test`)
+      stomp.unsubscribe(`/solution/${solutionId}`)
+      stomp.unsubscribe(`/solution/${solutionId}/test`)
     }
-  }, [stomp.isConnected, userContext.user?.login])
+  }, [stomp.isConnected, solutionId])
 
   const value = React.useMemo(() => ({ solutionId, setSolutionId }), [solutionId, setSolutionId])
 
